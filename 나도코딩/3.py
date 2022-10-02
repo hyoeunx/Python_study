c22f561591fa938135f7ae161d85d6ef3836aec8
hyoeunx <shimhe.06@gmail.com>
2022-10-02 23:58:42 +0900

    1002

---
+++ 나도코딩/3.py
@@ -0,0 +1,12 @@
+# 숫자 자료형
+print(1)
+print(3.14)
+
+# 문자 자료형
+print('hello world')
+print("안녕하세요")
+print('10')
+
+# 불리안 자료형(대문자)
+print(True)
+print(False)
---
+++ 나도코딩/4.py
@@ -0,0 +1,8 @@
+# 변수
+a = 10
+b = 20
+c = '하이'
+
+print(a)
+print(b)
+print(c)
