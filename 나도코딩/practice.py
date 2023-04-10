72f3fc6d2d23a1fa179d09828faaa1465366adbd
hyoeunx <shimhe.06@gmail.com>
2023-04-10 23:57:05 +0900

    0410

--- 나도코딩/practice.py
+++ 나도코딩/practice.py
@@ -1,7 +1,15 @@
-print('hello'+'wolrd')
-print('hello', 'world')
-print('hello world')
-print('hello '*3)
+# print('hello'+'wolrd')
+# print('hello', 'world')
+# print('hello world')
+# print('hello '*3)
 
-for i in range(10): #0~9
-    print(f'{i}번째')
+# for i in range(10): #0~9
+#     print(f'{i}번째')
+    
+
+num = input('번호입력 : ');   
+
+if(num.startswith('1')):
+    print('1학년입니다')
+elif(num.startwith('2')):
+    print('2학년입니다')
